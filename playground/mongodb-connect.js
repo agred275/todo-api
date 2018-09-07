aeea2e07a2f1178476c1e95d59154541d7426d88
Kedarnath Bharadwaj <agresh.bharadwaj@student.manchester.ac.uk>
2018-09-07 14:04:20 -0400

    Post todos

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,7 +1,7 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
   }
@@ -31,5 +31,5 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log(result.ops[0]._id.getTimestamp());
   // });
 
-  db.close();
+  client.close();
 });
